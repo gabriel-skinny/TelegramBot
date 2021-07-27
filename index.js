5cfe89cf7c9f3cee23c011edef02dfd4dce5ebcd
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-07-27 13:49:59 -0300

    Handling errors

--- index.js
+++ index.js
@@ -7,30 +7,33 @@ const token = '1933388008:AAEWIx74qMfEHjSEFFL5mBlv9SCouZUfKcQ';
 
 const bot = new TelegramBot(token, {polling: true});
 
-
-bot.onText(/\/echo (.+)/, (msg, match) => {
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; 
-
+function download(url, dest, cb) {
+  const file = fs.createWriteStream(dest);
   
-  bot.sendMessage(chatId, resp);
-});
+  http.get(url, response => {
+    response.pipe(file);
+
+    file.on("finish", () => {
+      file.close(cb);
+    })
+  }).on("error", err => {
+      fs.unlink(dest);
+      if (cb) cb(err.message);
+  })
+}
 
 bot.on('message', async (msg) => {
   const chatId = msg.chat.id;
 
 
   if (msg.photo) {
-
     const arrayLength = msg.photo.length;
 
-    const file = fs.createWriteStream(`./photos/${msg.caption}.jpeg`)
     const fileLink = await bot.getFileLink(msg.photo[arrayLength-1].file_id);
 
-    http.get(`${fileLink}`, (response) => {
-      response.pipe(file);
-    });
+    download(fileLink, `./photos/${msg.caption}.jpeg`, msg => {
+      console.log(msg || "Tudo Okay");
+    })
 
     bot.sendMessage(chatId, 'Arquivo baixado');
   }

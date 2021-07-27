e9efb3c0dbf428174603fdf7d30b3545e315a315
gabriel-skinny <gabrielcatoni12@gmail.com.br>
2021-07-27 15:06:54 -0300

    Handling errors

--- index.js
+++ index.js
@@ -7,19 +7,41 @@ const token = '1933388008:AAEWIx74qMfEHjSEFFL5mBlv9SCouZUfKcQ';
 
 const bot = new TelegramBot(token, {polling: true});
 
-function download(url, dest, cb) {
-  const file = fs.createWriteStream(dest);
-  
-  http.get(url, response => {
-    response.pipe(file);
+function download(url, dest) {
+  return new Promise((resolve, reject) => {
+      const file = fs.createWriteStream(dest, { flags: "wx" });
 
-    file.on("finish", () => {
-      file.close(cb);
-    })
-  }).on("error", err => {
-      fs.unlink(dest);
-      if (cb) cb(err.message);
-  })
+      const request = http.get(url, response => {
+          if (response.statusCode === 200) {
+              response.pipe(file);
+          } else {
+              file.close();
+              fs.unlink(dest, () => {});
+              reject(`Server responded with ${response.statusCode}: ${response.statusMessage}`);
+          }
+      });
+
+      request.on("error", err => {
+          file.close();
+          fs.unlink(dest, () => {});
+          reject(err.message);
+      });
+
+      file.on("finish", () => {
+          resolve();
+      });
+
+      file.on("error", err => {
+          file.close();
+
+          if (err.code === "EEXIST") {
+              reject("File already exists");
+          } else {
+              fs.unlink(dest, () => {}); // Delete temp file
+              reject(err.message);
+          }
+      });
+  });
 }
 
 bot.on('message', async (msg) => {
@@ -31,9 +53,7 @@ bot.on('message', async (msg) => {
 
     const fileLink = await bot.getFileLink(msg.photo[arrayLength-1].file_id);
 
-    download(fileLink, `./photos/${msg.caption}.jpeg`, msg => {
-      console.log(msg || "Tudo Okay");
-    })
+    await download(fileLink, `./photos/${msg.caption}.jpeg`)
 
     bot.sendMessage(chatId, 'Arquivo baixado');
   }
